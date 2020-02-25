9861d3bb5fcf9f299e0728e62fa50cda9c444c16
Ryan Joo <ryanhjoo@gmail.com>
2020-02-25 05:44:45 -0800

    use getCurrentPosition from expo-location

--- components/Map.js
+++ components/Map.js
@@ -1,32 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
-import MapView, { PROVIDER_GOOGLE, Circle } from 'react-native-maps';
-
-const currentLocation = {
-  coords: {
-    longitude: -118.0292256922629,
-    latitude: 34.13063609510113,
-  }
-}
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 const Map = () => {
+  const [location, setLocation] = useState({
+    latitude: 0,
+    longitude: 0
+  });
+
+  const getLocation = async () => {
+    let position = await Location.getCurrentPositionAsync({});
+    setLocation({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
   return <MapView 
       style={styles.map} 
       provider={PROVIDER_GOOGLE}
       showsUserLocation={true}
-      // followsUserLocation={true}
-      initialRegion={{
-        ...currentLocation.coords,
+      followsUserLocation={true}
+      showCompass={true}
+      region={{
+        latitude: location.latitude,
+        longitude: location.longitude,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01
       }} 
-    >
-      {/* <Circle 
-        center={currentLocation.coords} 
-        radius={30} 
-        strokeColor="rgba(158, 158, 255, 1.0)" 
-        fillColor="rgba(158, 158, 255, 0.3)" /> */}
-    </MapView>
+    />
 };
 
 const styles = StyleSheet.create({
